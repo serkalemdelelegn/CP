c96865588ee9144fb3f6535c4c9688c64fd61da6
Serkalem Delelegn <140797915+serkalemdelelegn@users.noreply.github.com>
2024-08-31 23:23:47 +0300

    Time: 53 ms (90.35%), Space: 27.5 MB (72.97%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());  // Sort the array
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Skip duplicate elements for the first position
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    // Skip duplicate elements for the second position
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        ++left;
+                    }
+                    // Skip duplicate elements for the third position
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        --right;
+                    }
+                    ++left;
+                    --right;
+                } else if (sum < target) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+
+        return result;
+    }
+};
