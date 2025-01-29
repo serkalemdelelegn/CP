1859238d68619531d12ff02450487841042f928d
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2025-01-30 02:32:26 +0300

    Time: 0 ms (100%), Space: 18.1 MB (76.43%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def find(x: int) -> int:
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+
+        p = list(range(len(edges)))
+        for a, b in edges:
+            pa, pb = find(a - 1), find(b - 1)
+            if pa == pb:
+                return [a, b]
+            p[pa] = pb
